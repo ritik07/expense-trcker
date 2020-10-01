605ef7696c954b11493e6faf39df7ac1f1425bc7
prateek sharma <52008115+prateek-sha@users.noreply.github.com>
2020-10-01 22:32:00 +0530

    Added react-router

--- src/App.js
+++ src/App.js
@@ -6,20 +6,24 @@ import { IncomeExpense } from './components/IncomeExpense';
 import { TransactionList } from './components/TransactionList';
 import { AddTransaction } from './components/AddTransaction';
 import GlobalProvider from './context/GlobalState';
+import {browserrouter as router , route , Link , Switch} from 'react-router-dom';
 
 function App() {
   return (
-    
+    <Router>
+    <Switch>
     <GlobalProvider>
       <Header/> 
       <div className="container">
-        <Balance/>
+    <Route to='/' excat
+        ><Balance/></Route>
         <IncomeExpense/>
         <TransactionList/>
         <AddTransaction/>
       </div>
     </GlobalProvider>
-  
+    <Switch>
+    </Router>
   );
 }
 
